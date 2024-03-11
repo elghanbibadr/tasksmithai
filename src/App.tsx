3c94399fb1bedd3aa21d8d3882e1551e6fe4536f
elghanbibadr <bghanbi50@gmail.com>
2024-03-11 15:20:24 +0100

    created the outer and inner circe

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
 import React from 'react'
-
+import { Flex, Heading, Spacer ,Box,Image,Text} from '@chakra-ui/react'
+import innerImg from "../src/assets/InnerCircleImg.svg"
+import innerImg2 from "../src/assets/cb-dev-logo 4.png"
 const App = () => {
   return (
-    <div>App</div>
+    <Box display="flex"  >
+     
+
+      <Flex align="center" justifySelf="end" gap="4">
+        
+     <Box  height="540px" width="540px" position='relative'>
+     <Image src={innerImg} />
+     <Text position='absolute' right='100px'  bottom='280px' textAlign="center"  className='link' fontWeight="500"  style={{ fontSize: "24px" }}  >Workflows</Text>
+
+     </Box>       
+      </Flex>
+
+
+
+    </Box>
   )
 }
 
