20f36e2b89202967efcc90f28644b84d48e7c0ea
elghanbibadr <bghanbi50@gmail.com>
2024-03-12 14:12:43 +0100

    created the first arc

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react'
-import { Flex, Heading, Spacer ,Box,Image,Text} from '@chakra-ui/react'
+import React, { useState } from 'react'
+import { Flex, Heading, Spacer, Box, Image, Text } from '@chakra-ui/react'
 import innerImg from "../src/assets/InnerCircleImg.svg"
 import innerImg2 from "../src/assets/cb-dev-logo 4.png"
+import settingIcon from "../src/assets/settingIcon.png"
+import smallCircleIcon from "../src/assets/smallCircle.svg"
+
+import menuIcon from "../src/assets/menuIcon.png"
+import { motion } from 'framer-motion'
+import line from "../src/assets/line.svg"
+import arc1 from "../src/assets/Group 13.svg"
+
 const App = () => {
+  const [isHovered, setIsHovered] = useState(true);
+
   return (
-    <Box display="flex"  >
-     
+    <>
+  <Box transform='scale(0.7)' zIndex='999'  height="680px" width="680px" position='relative'>
+  <Image src={smallCircleIcon} />
 
-      <Flex align="center" justifySelf="end" gap="4">
-        
-     <Box  height="540px" width="540px" position='relative'>
-     <Image src={innerImg} />
-     <Text position='absolute' right='100px'  bottom='280px' textAlign="center"  className='link' fontWeight="500"  style={{ fontSize: "24px" }}  >Workflows</Text>
+{ isHovered &&  <motion.img  src={arc1}  className='circle'/>
+}  <Text position='absolute' right='34px' bottom='260px' textAlign="center" className='link' fontWeight="500" style={{ fontSize: "22px" }}  >Workflows</Text>
+     {  <Text position='absolute' left='60px' bottom='260px' textAlign="center" className='link' fontWeight="500" style={{ fontSize: "22px" }}    onMouseEnter={() => setIsHovered(true)} // Set isHovered to true on mouse enter
+        onMouseLeave={() => setIsHovered(false)} >Models</Text>}
+       <Text position='absolute' left='184px' top='80px' textAlign="center" className='link' fontWeight="500" style={{ fontSize: "22px" }}  >Workspaces</Text>
+       <Text position='absolute' left='220px' bottom='140px' textAlign="center" className='link' fontWeight="500" style={{ fontSize: "22px" }}  >Data</Text>
 
-     </Box>       
-      </Flex>
+</Box>
+    </>
+ 
 
-
-
-    </Box>
   )
 }
 
